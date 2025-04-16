7aadf21835a8753180068c4dcd27ec340af705a6
Dizzy <dizzysaxophone89@gmail.com>
2025-04-16 16:14:35 -0400

    C: Draws player now too

--- player.py
+++ player.py
@@ -1,12 +1,11 @@
+import pygame
 from circleshape import *
+from constants import *
 
 class Player(CircleShape):
-    def __init__(self, x, y, PLAYER_RADIUS):
-        super().__init__(self)
-        self.x = x
-        self.y = y
-        rotation = 0
-        self.radius = PLAYER_RADIUS 
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -15,3 +14,7 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c] 
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        
