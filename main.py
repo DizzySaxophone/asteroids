743c5a3a58db99e1b73329866edb1f2c66dba179
Dizzy <dizzysaxophone89@gmail.com>
2025-04-17 01:17:30 -0400

    D: Player now moves!

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        
+        
+        # limit the frame rate to 60 fps
         dt = clock.tick(60)/1000
     
 if __name__ == "__main__":
